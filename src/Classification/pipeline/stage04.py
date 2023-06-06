0c354f8ab242bfb1766b773a2122327c381674e8
kimty15 <ty44976@gmail.com>
2023-06-07 00:05:48 +0700

    Done pipeline

---
+++ src/Classification/pipeline/stage04.py
@@ -0,0 +1,31 @@
+from Classification.config.configuration import ConfigurationManager
+from Classification.components.evaluation import Evaluation
+from Classification import logging
+import sys
+from Classification.exception import CustomExpection
+STAGE_NAME = "Evaluation stage"
+
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        val_config = config.get_validation_config()
+        evaluation = Evaluation(val_config)
+        evaluation.evaluation()
+        evaluation.save_score()
+
+
+
+if __name__ == '__main__':
+    try:
+        logging.info(f"*******************")
+        logging.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logging.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logging.error(f"Error in stage {STAGE_NAME}")
+        raise CustomExpection(e,sys)
--- src/Classification/utils/common.py
+++ src/Classification/utils/common.py
@@ -2,13 +2,15 @@ import os
 # from box.exceptions import BoxValueError
 import yaml
 from Classification import logging
-from Classification.exception import CustomExpection
+import json
+import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
 from pathlib import Path
 from typing import Any
+import base64
+from Classification.exception import CustomExpection
 import sys
-
 
 
 @ensure_annotations
@@ -67,3 +69,107 @@ def get_size(path: Path) -> str:
     """
     size_in_kb = round(os.path.getsize(path)/1024)
     return f"~ {size_in_kb} KB"
+
+@ensure_annotations
+def save_json(path: Path, data: dict):
+    """save json data
+
+    Args:
+        path (Path): path to json file
+        data (dict): data to be saved in json file
+    """
+    try:
+        with open(path, "w") as f:
+            json.dump(data, f, indent=4)
+
+        logging.info(f"json file saved at: {path}")
+    except Exception as e:
+        logging.error("Error occured in saving json file")
+        raise CustomExpection(e,sys)
+
+
+
+
+@ensure_annotations
+def load_json(path: Path) -> ConfigBox:
+    """load json files data
+
+    Args:
+        path (Path): path to json file
+
+    Returns:
+        ConfigBox: data as class attributes instead of dict
+    """
+    try:
+        with open(path) as f:
+            content = json.load(f)
+
+        logging.info(f"json file loaded succesfully from: {path}")
+        return ConfigBox(content)
+    except Exception as e:
+        logging.error("Error occured in loading json file")
+        raise CustomExpection(e,sys)
+
+
+@ensure_annotations
+def save_bin(data: Any, path: Path):
+    """save binary file
+
+    Args:
+        data (Any): data to be saved as binary
+        path (Path): path to binary file
+    """
+    try:
+        joblib.dump(value=data, filename=path)
+        logging.info(f"binary file saved at: {path}")
+    except Exception as e:
+        logging.error("Error occured in saving binary file")
+        raise CustomExpection(e,sys)
+
+
+@ensure_annotations
+def load_bin(path: Path) -> Any:
+    """load binary data
+
+    Args:
+        path (Path): path to binary file
+
+    Returns:
+        Any: object stored in the file
+    """
+    try:
+        data = joblib.load(path)
+        logging.info(f"binary file loaded from: {path}")
+        return data
+    except Exception as e:
+        logging.error("Error occured in loading binary file")
+        raise CustomExpection(e,sys)
+
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    try:
+        size_in_kb = round(os.path.getsize(path)/1024)
+        return f"~ {size_in_kb} KB"
+    except Exception as e:
+        logging.error("Error occured in getting size of file")
+        raise CustomExpection(e,sys)
+
+
+def decodeImage(imgstring, fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
